12de9ec501213a4997fe088929d4362de84dd378
Nasuhi Bercin <nas@Ahmets-MacBook-Air.local>
2020-02-12 11:47:38 +0000

    add new build

---
+++ client/build/precache-manifest.102d7727f85676e925409f711e7dd27e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33c03e7a72f5006df43073fc74a4a99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6536324f0e3880ad3577",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "497f8c4e6007d1bfd7d9",
+    "url": "/static/js/2.ec2d324a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ec2d324a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6536324f0e3880ad3577",
+    "url": "/static/js/main.53d25403.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
